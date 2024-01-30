54f994a6577627b7a25028fa8db20a8e69f28e73
simanthiniguleria <156087701+simanthiniguleria@users.noreply.github.com>
2024-01-30 19:59:56 +0530

    changes to responsiveness

--- responsive.js
+++ responsive.js
@@ -1,11 +1,7 @@
-$(document).ready(function(){
-    $(".controls-line").click(function(event)
-    {
-        event.preventDefault();
-        $(".menu").toggle();
-    });
-});
+$('.controls').click(function(){
+    $('.line1').toggleClass("line1-transform");
+    $('.line2').toggleClass("line2-transform");
+    $('.line3').toggleClass("line3-transform");
+    $('.toggle').toggleClass("toggle-active");
+})
 
-
-  
-  
